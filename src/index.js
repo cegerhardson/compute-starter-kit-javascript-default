070a0b7e7414e7cfac5b38bf7aa2276ef4d9596c
Chloe Gerhardson <66563430+cegerhardson@users.noreply.github.com>
2023-05-31 10:11:43 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,82 @@
+//! Default Compute@Edge template program.
+
+/// <reference types="@fastly/js-compute" />
+// import { CacheOverride } from "fastly:cache-override";
+// import { Logger } from "fastly:logger";
+import { env } from "fastly:env";
+import { includeBytes } from "fastly:experimental";
+
+// Load a static file as a Uint8Array at compile time.
+// File path is relative to root of project, not to this file
+const welcomePage = includeBytes("./src/welcome-to-compute@edge.html");
+
+// The entry point for your application.
+//
+// Use this fetch event listener to define your main request handling logic. It could be
+// used to route based on the request properties (such as method or path), send
+// the request to a backend, make completely new requests, and/or generate
+// synthetic responses.
+
+addEventListener("fetch", (event) => event.respondWith(handleRequest(event)));
+
+async function handleRequest(event) {
+  // Log service version
+  console.log("FASTLY_SERVICE_VERSION:", env('FASTLY_SERVICE_VERSION') || 'local');
+  
+  // Get the client request.
+  let req = event.request;
+
+  // Filter requests that have unexpected methods.
+  if (["POST", "PUT", "PATCH", "DELETE"].includes(req.method)) {
+    return new Response("This method is not allowed", {
+      status: 405,
+    });
+  }
+
+  let url = new URL(req.url);
+
+  // If request is to the `/` path...
+  if (url.pathname == "/") {
+    // Below are some common patterns for Compute@Edge services using JavaScript.
+    // Head to https://developer.fastly.com/learning/compute/javascript/ to discover more.
+
+    // Create a new request.
+    // let bereq = new Request("http://example.com");
+
+    // Add request headers.
+    // req.headers.set("X-Custom-Header", "Welcome to Compute@Edge!");
+    // req.headers.set(
+    //   "X-Another-Custom-Header",
+    //   "Recommended reading: https://developer.fastly.com/learning/compute"
+    // );
+
+    // Create a cache override.
+    // To use this, uncomment the import statement at the top of this file for CacheOverride.
+    // let cacheOverride = new CacheOverride("override", { ttl: 60 });
+
+    // Forward the request to a backend.
+    // let beresp = await fetch(req, {
+    //   backend: "backend_name",
+    //   cacheOverride,
+    // });
+
+    // Remove response headers.
+    // beresp.headers.delete("X-Another-Custom-Header");
+
+    // Log to a Fastly endpoint.
+    // To use this, uncomment the import statement at the top of this file for Logger.
+    // const logger = new Logger("my_endpoint");
+    // logger.log("Hello from the edge!");
+
+    // Send a default synthetic response.
+    return new Response(welcomePage, {
+      status: 200,
+      headers: new Headers({ "Content-Type": "text/html; charset=utf-8" }),
+    });
+  }
+
+  // Catch all other requests and return a 404.
+  return new Response("The page you requested could not be found", {
+    status: 404,
+  });
+}
